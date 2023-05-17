94c8110398e67b65c5d278b1f6d53ddb632b0e5d
Omar Balde <116875415+omwcoding@users.noreply.github.com>
2023-05-18 01:32:09 +0200

    creata pagina scelta e fixato bottone #todo fixare routing scelta.php

--- TestYii/basic/views/site/index.php
+++ TestYii/basic/views/site/index.php
@@ -10,7 +10,8 @@ $this->title = 'My Yii Application';
     <div class="jumbotron text-center bg-transparent">
         <h1 class="display-4">Benvenuto nel sito web del gruppo INDI!</h1>
         <p class="lead">Entra con le tue credenziali e scopri i servizi che offriamo.</p>
-        <p><a class="btn btn-lg btn-success" href="">Entra in XXX</a></p>
+        <p><a class="btn btn-lg btn-success" href="<?= Yii::$app->urlManager->createUrl(['scelta.php']) ?>">Entra in XXX</a></p>
+
     </div>
     <div class="body-content">
     </div>
---
+++ TestYii/basic/views/site/scelta.php
@@ -0,0 +1,11 @@
+<?php
+/** @var yii\web\View $this */
+
+$this->title = 'Scelta';
+
+?>
+
+<div class="site-scelta">
+    <h1>Questa è la pagina di scelta</h1>
+    <p>Qui puoi fare la tua scelta preferita.</p>
+</div>
