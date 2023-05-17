0412991b8be4829d11200ca8b28b388b038374e2
Omar Balde <116875415+omwcoding@users.noreply.github.com>
2023-05-17 08:39:51 +0200

    prima commit

---
+++ TestYii/basic/views/site/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application';
+
+?>
+
+<div class="site-index">
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="display-4">Benvenuto nel sito web del gruppo INDI!</h1>
+        <p class="lead">Entra con le tue credenziali e scopri i servizi che offriamo.</p>
+        <p><a class="btn btn-lg btn-success" href="login.php">Entra in XXX</a></p>
+    </div>
+    <div class="body-content">
+    </div>
+</div>
